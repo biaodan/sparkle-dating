aac4711677994f37599a47d62c87ae2516499d45
biaodan <g07788754@gmail.com>
2026-07-04 18:27:29 +0800

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('go').onclick=()=>{
-const s=document.getElementById('state').value;
-if(!s){alert('Please select your state');return;}
-window.location='https://svip1027.top/KFDG556?state='+encodeURIComponent(s)+'&looking=women';
-};

8709be3517c52bc4933afa1dffd3a57fc0b72d7b
biaodan <g07788754@gmail.com>
2026-07-04 17:57:12 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('go').onclick=()=>{
+const s=document.getElementById('state').value;
+if(!s){alert('Please select your state');return;}
+window.location='https://svip1027.top/KFDG556?state='+encodeURIComponent(s)+'&looking=women';
+};
